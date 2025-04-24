88a0372354a16b814bf85b1e94b75c91589c7a46
shreddykr <148612874+shreddykr@users.noreply.github.com>
2025-04-23 21:46:31 -0700

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function()
+    print("\27[32m") -- Green text
+    print([[
+
+    /)  /)  ~ ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+   ( •-• )  ~ ♡ Thank you for installing   ♡
+   /づづ    ~  ♡  skr-dispatch! ♡          ♡
+             ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+    ]])
+    print("\27[0m") -- Reset text color
+end)
